f34f5771153deeb8e9a78f4c865f6a8688a4ed47
Carlos Nalli <cnalli@gmail.com>
2025-04-24 19:50:49 -0300

    Corrigindo variável de porta e atualizando .gitignore

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,7 +2,7 @@
 const app = require('./server');
 
 // Iniciar o servidor na porta definida no render, o troço que deu erro.
-const port = process.env.PORT || 10000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Servidor HoriMinas rodando na porta ${port}`);
