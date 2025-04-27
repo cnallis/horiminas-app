d3928129ecf8e5d5a77f2e050693fa9453ad4d3e
Carlos Nalli <cnalli@gmail.com>
2025-04-27 14:58:19 -0300

    Alteração de db para teste

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,3 +1,8 @@
+if (!process.env.DB_NAME) {
+  console.error('Variável de ambiente DB_NAME não configurada!');
+  process.exit(1);
+}
+
 const { Pool } = require('pg');
 require('dotenv').config();
 
@@ -5,7 +10,7 @@ require('dotenv').config();
 const pool = new Pool({
   user: process.env.DB_USER || 'postgres',
   host: process.env.DB_HOST || 'localhost',
-  database: process.env.DB_NAME || 'horiminas',
+  database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD || 'postgres',
   port: process.env.DB_PORT || 5432,
   ssl: {
